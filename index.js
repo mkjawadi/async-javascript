c79068fc66a36e19bed94efac1a57975fbbf524a
Kumail Jawadi <mk.jawadi@hotmail.com>
2018-07-19 15:16:42 +0430

    Implement promise pattern for async code.

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
+// Promise-based approach
 console.log('Before');
-getUser(1, (user)=> {
-    console.log('User: ', user);
-    getRepositories(user.gitHubUsername, (repos)=> {
-        console.log('Repositories: ', repos);
-    });
-});
+
+getUser(1)
+  .then(user => getRepositories(user.gitHubUsername))
+  .then(repos => getCommits(repos[0]))
+  .then(commits => console.log('Commits', commits))
+  .catch(err => console.log('Error', err.message));
+
+
 console.log('After');
 
-function getUser(id, callback){
-    setTimeout(()=> {
-        console.log('Reading a user from a database...');
-        callback({id: id, gitHubUsername: 'kumail'});
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    // Kick off some async work 
+    setTimeout(() => {
+      console.log('Reading a user from a database...');
+      resolve({ id: id, gitHubUsername: 'mosh' });
     }, 2000);
+  });
 }
 
-function getRepositories(username, callback){
-    setTimeout(()=> {
-        console.log('Calling GitHub API...');
-        callback(['Repo1','Repo2','Repo3']);
-    }, 2000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['repo1', 'repo2', 'repo3']);
+    }, 2000);  
+  });
 }
+
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['commit']);
+    }, 2000);
+  });
+}
+
+
+// Callback based approach
+// console.log('Before');
+// getUser(1, (user) => {
+//   getRepositories(user.gitHubUsername, (repos) => {
+//     getCommits(repos[0], (commits) => {
+//       console.log(commits);
+//     })
+//   })
+// });
+// console.log('After');
+
+// function getUser(id, callback) {
+//   setTimeout(() => {
+//     console.log('Reading a user from a database...');
+//     callback({ id: id, gitHubUsername: 'mosh' });
+//   }, 2000);
+// }
+
+// function getRepositories(username, callback) {
+//   setTimeout(() => {
+//     console.log('Calling GitHub API...');
+//     callback(['repo1', 'repo2', 'repo3']);
+//   }, 2000);
+// }
+
+// function getCommits(repo, callback) {
+//   setTimeout(() => {
+//     console.log('Calling GitHub API...');
+//     callback(['commit']);
+//   }, 2000);
+// }
